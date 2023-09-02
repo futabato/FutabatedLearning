766ea1bb4dd9d38637b150e11afb91083dac3258
futabato <01futabato10@gmail.com>
2023-09-02 11:13:38 +0900

    :fire: Remove run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python src/federatedlearning/train.py --gpu 0 \
-    --num_epochs 200 --lr 0.05 --batch_size 100 --num_workers 20 \
-    --num_byzantines 8 --byzantine_type bitflip \
-    --rho_ratio 200 --num_trimmed_values 12 --zeno_size 4
