d739e5a377a1e289abc1b15ba69691902d648185
futabato <01futabato10@gmail.com>
2023-09-04 19:59:45 +0900

    :white_check_mark: Add a test about bitflip attack

--- tests/test_byzantine_attack.py
+++ tests/test_byzantine_attack.py
@@ -1,17 +1,19 @@
 import torch
 
-from attack.byzantines import no_byzantine, labelflip_attack
+from attack.byzantines import bitflip_attack, labelflip_attack, no_byzantine
 
 
 def test_no_byzantine():
-    expected = [torch.tensor([1.0, 2.0, 3.0]), torch.tensor([4.0, 5.0, 6.0])]
+    tensor = torch.tensor([1.0, 2.0, 3.0], device="cpu")
+    expected = torch.tensor([1.0, 2.0, 3.0], device="cpu")
     f = 0
 
     # Byzantine Attack
-    actual = no_byzantine(expected, f)
+    actual = no_byzantine(tensor, f)
 
     # Verify result
-    assert actual == expected
+    assert actual.shape == expected.shape
+    assert torch.equal(actual, expected)
 
 
 def test_labelflip_attack():
@@ -26,6 +28,87 @@ def test_labelflip_attack():
     assert torch.equal(actual, expected)
 
 
+def test_bitflip_attack():
+    v = [
+        torch.tensor([1, 1, 0, 0]),
+        torch.tensor([1, 0, 1, 0]),
+        torch.tensor([1, 0, 0, 1]),
+        torch.tensor([0, 1, 1, 0]),
+        torch.tensor([0, 1, 0, 1]),
+        torch.tensor([0, 0, 1, 1]),
+    ]
+    expected = [
+        # num_byzantine = 0
+        [
+            torch.tensor([1, 1, 0, 0]),
+            torch.tensor([1, 0, 1, 0]),
+            torch.tensor([1, 0, 0, 1]),
+            torch.tensor([0, 1, 1, 0]),
+            torch.tensor([0, 1, 0, 1]),
+            torch.tensor([0, 0, 1, 1]),
+        ],
+        # num_byzantine = 1
+        [
+            torch.tensor([0, 0, 1, 1]),
+            torch.tensor([1, 0, 1, 0]),
+            torch.tensor([1, 0, 0, 1]),
+            torch.tensor([0, 1, 1, 0]),
+            torch.tensor([0, 1, 0, 1]),
+            torch.tensor([0, 0, 1, 1]),
+        ],
+        # num_byzantine = 2
+        [
+            torch.tensor([0, 0, 1, 1]),
+            torch.tensor([0, 1, 0, 1]),
+            torch.tensor([1, 0, 0, 1]),
+            torch.tensor([0, 1, 1, 0]),
+            torch.tensor([0, 1, 0, 1]),
+            torch.tensor([0, 0, 1, 1]),
+        ],
+        # num_byzantine = 3
+        [
+            torch.tensor([0, 0, 1, 1]),
+            torch.tensor([0, 1, 0, 1]),
+            torch.tensor([0, 1, 1, 0]),
+            torch.tensor([0, 1, 1, 0]),
+            torch.tensor([0, 1, 0, 1]),
+            torch.tensor([0, 0, 1, 1]),
+        ],
+        # num_byzantine = 4
+        [
+            torch.tensor([0, 0, 1, 1]),
+            torch.tensor([0, 1, 0, 1]),
+            torch.tensor([0, 1, 1, 0]),
+            torch.tensor([1, 0, 0, 1]),
+            torch.tensor([0, 1, 0, 1]),
+            torch.tensor([0, 0, 1, 1]),
+        ],
+        # num_byzantine = 5
+        [
+            torch.tensor([0, 0, 1, 1]),
+            torch.tensor([0, 1, 0, 1]),
+            torch.tensor([0, 1, 1, 0]),
+            torch.tensor([1, 0, 0, 1]),
+            torch.tensor([1, 0, 1, 0]),
+            torch.tensor([0, 0, 1, 1]),
+        ],
+        # num_byzantine = 6
+        [
+            torch.tensor([0, 0, 1, 1]),
+            torch.tensor([0, 1, 0, 1]),
+            torch.tensor([0, 1, 1, 0]),
+            torch.tensor([1, 0, 0, 1]),
+            torch.tensor([1, 0, 1, 0]),
+            torch.tensor([1, 1, 0, 0]),
+        ],
+    ]
+    for f in range(len(v) + 1):
+        actual = bitflip_attack(v, f)
+        assert len(actual) == len(expected[f])
+        assert torch.equal(actual[0], expected[f][0])
+
+
 if __name__ == "__main__":
     test_no_byzantine()
     test_labelflip_attack()
+    test_bitflip_attack()
