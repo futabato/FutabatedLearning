8dbc0cd5e1190182b260cb3e594d1de13f5c9705
futabato <01futabato10@gmail.com>
2023-09-06 00:40:23 +0900

    :recycle: Refactor code

--- src/attack/byzantines.py
+++ src/attack/byzantines.py
@@ -1,53 +1,65 @@
 import torch
 
 
-def no_byzantine(v, f):
+def no_byzantine(
+    gradients_list: list[torch.Tensor], _: int
+) -> list[torch.Tensor]:
     """no faulty workers
 
     Args:
-        v (_type_): gradients
-        f (int): num_byzantines
+        gradients_list (list[torch.Tensor]): gradients tensor list
+        _ (int): number of byzantines
     """
-    return v
+    return gradients_list
 
 
-def gaussian_attack(v, f: int):
+def gaussian_attack(
+    gradients_list: list[torch.Tensor], num_byzantines: int
+) -> list[torch.Tensor]:
     """failures that add Gaussian noise
 
     Args:
-        v (_type_): gradients
-        f (int): num_byzantines
+        gradients_list (list[torch.Tensor]): gradients tensor list
+        num_byzantines (int): number of byzantines
+
+    Returns:
+        list[torch.Tensor]: gradients tensor list added gaussian noise
     """
-    for i in range(f):
-        v[i] = torch.randn(v[i].size()) * 200
+    for i in range(num_byzantines):
+        gradients_list[i] = torch.randn(gradients_list[i].size()) * 200
+    return gradients_list
 
 
-def bitflip_attack(v: list[torch.Tensor], f: int) -> list[torch.Tensor]:
+def bitflip_attack(
+    gradients_list: list[torch.Tensor], num_byzantines: int
+) -> list[torch.Tensor]:
     """bit-flipping failure
 
     Args:
-        v (list[torch.Tensor]): gradients
-        f (int): num_byzantines
+        gradients_list (list[torch.Tensor]): gradients tensor list
+        num_byzantines (int): number of byzantines
 
     Returns:
-        list[torch.Tensor]: bit flipped data
+        list[torch.Tensor]: bit flipped gradients tensor list
     """
-    flipped_v = []
-    for worker, tensor in enumerate(v):
-        flipped_tensor = tensor.clone()
-        if worker < f:
-            flipped_tensor.view(-1)[:] = 1 - flipped_tensor.view(-1)[:]
-        flipped_v.append(flipped_tensor)
-    return flipped_v
+    bitflipped_gradients_list = []
+    for worker_idx, gradients_tensor in enumerate(gradients_list):
+        flipped_gradients_tensor = gradients_tensor.clone()
+        if worker_idx < num_byzantines:
+            flipped_gradients_tensor.view(-1)[:] = (
+                1 - flipped_gradients_tensor.view(-1)[:]
+            )
+        bitflipped_gradients_list.append(flipped_gradients_tensor)
+    return bitflipped_gradients_list
 
 
 def labelflip_attack(label: torch.Tensor) -> torch.Tensor:
     """label-flipping failure
 
     Args:
-        label (torch.Tensor): label data
+        label (torch.Tensor): label tensor
 
     Returns:
-        torch.Tensor: flipped label data
+        torch.Tensor: flipped label tensor
     """
     return 9 - label
