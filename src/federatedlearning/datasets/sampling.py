cb79845a7f84ab87063e7e30eccdc3420dafc42a
futabato <01futabato10@gmail.com>
2024-01-30 08:55:20 +0900

    :bug: Fix a bug
    how to access cifar10 dataset label

--- src/federatedlearning/datasets/sampling.py
+++ src/federatedlearning/datasets/sampling.py
@@ -169,8 +169,7 @@ def cifar_noniid(dataset, num_users):
     idx_shard = [i for i in range(num_shards)]
     dict_users = {i: np.array([]) for i in range(num_users)}
     idxs = np.arange(num_shards*num_imgs)
-    # labels = dataset.train_labels.numpy()
-    labels = np.array(dataset.train_labels)
+    labels = np.array(dataset.targets)
 
     # sort labels
     idxs_labels = np.vstack((idxs, labels))
