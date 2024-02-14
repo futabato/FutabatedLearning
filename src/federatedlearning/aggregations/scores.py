f5c620082e2a178ddf279a21021aa8dbad48d16c
futabato <01futabato10@gmail.com>
2024-02-15 06:20:36 +0900

    :construction: Hide calc_sum_distances func (temporary)

--- src/federatedlearning/aggregations/scores.py
+++ src/federatedlearning/aggregations/scores.py
@@ -1,11 +1,11 @@
-import math
+# import math
 
-import torch
+# import torch
 
 
-def calc_sum_distances(gradient, v, f):
-    if 2 * f + 2 > v.shape[1]:
-        f = int(math.floor((v.shape[1] - 2) / 2.0))
-    num_neighbours = v.shape[1] - 2 - f
-    sorted_distance = torch.sum((v - gradient) ** 2, dim=0).sort()[0]
-    return torch.sum(sorted_distance[1 : (1 + num_neighbours)]).item()
+# def calc_sum_distances(gradient, v, f):
+#     if 2 * f + 2 > v.shape[1]:
+#         f = int(math.floor((v.shape[1] - 2) / 2.0))
+#     num_neighbours = v.shape[1] - 2 - f
+#     sorted_distance = torch.sum((v - gradient) ** 2, dim=0).sort()[0]
+#     return torch.sum(sorted_distance[1 : (1 + num_neighbours)]).item()
